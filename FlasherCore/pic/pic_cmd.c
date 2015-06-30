87a9ce9dc58c796c7d18d4180b2ac25004b5ed4e
Sergey Savchuk <savchuk.sergey@gmail.com>
2015-06-30 22:40:34 +0300

    pic commands

---
+++ FlasherCore/pic/pic_cmd.c
@@ -0,0 +1,4 @@
+#include "../settings.h"
+#include <util/delay.h>
+#include "../io/io.c"
+#include "pic_cmd.h"
---
+++ FlasherCore/pic/pic_cmd.h
@@ -0,0 +1,8 @@
+#ifndef PIC_COMMANDS_H_
+#define PIC_COMMANDS_H_
+
+#include "../settings.h"
+#include <util/delay.h>
+
+
+#endif
